779fff4175844e6b23f13dd464e346b1d4dae04d
Abhinav-Devkar7794 <devkarabhinav@gmail.com>
2020-05-18 09:04:51 +0100

    spring angular

---
+++ src/main/java/com/javapoint/server/main/GitSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javapoint.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringDemoApplication.class, args);
+	}
+
+}
